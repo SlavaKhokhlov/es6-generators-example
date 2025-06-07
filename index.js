0b09663e3e8b00f7ae4555edcb7fbc029f6fdc0a
Slava Khokhlov <slava.khokhlov@gmail.com>
2025-06-07 12:04:32 +1000

    added output results

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function* idMaker() {
 
 var gen = idMaker();
 
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
